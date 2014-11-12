c17a47187785bddcb7ca034140f0d37dbe0d6ea8
Daniel Ortiz <daniel@makrplace.com>
2014-11-12 08:17:12 -0500

    Addition of a travis file, updates to the package.json, and a very basic gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+/**
+ * This grunt file compiles and combines all of the client-side
+ * content.
+ */
+'use strict';
+
+// Setup the paths for the files
+var paths = {
+	js:['*.js'],
+	html:['public/views'],
+	css:['!bower_components/**']
+};
+
+// Export the grunt file. 
+module.exports = function(grunt){
+	
+}
+
+
