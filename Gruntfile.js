35f3caca6ccbedac55e09d1869e9c36a2d2b0756
Daniel Ortiz <danielor@umich.edu>
2014-11-13 09:19:13 -0500

    Addition of a basic wireframe of a grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,29 @@
  */
 'use strict';
 
-// Setup the paths for the files
+//Setup the paths for the files
 var paths = {
 	js:['*.js'],
 	html:['public/views'],
 	css:['!bower_components/**']
 };
 
+/**
+ * A function that setups the grunt task to build and validate
+ * all of the front-end assets
+ * @function initializeGrunt
+ * @param grunt {object} The grunt object
+ */
+function initializeGrunt(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		assets: grunt.file.readJSON("presentation.json")
+	})
+}	
+
 // Export the grunt file. 
-module.exports = function(grunt){
-	
-}
+module.exports = initializeGrunt;
+
+
 
 
